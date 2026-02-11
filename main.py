c46cbe72232290e3437e67055be597f4d0b3a52e
imporeto-debug <imporeto@gmail.com>
2026-02-11 16:43:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ async def send_wife_message(topic: str):
     ]
     content = await ask_deepseek(prompt, max_tokens=MAX_RESPONSE_TOKENS_SHORT)
     if content:
-        await channel.send(f"<@{WIFE_ID}> {content}")
+        await channel.send(f"{affectionate_name}, {content}")
 
 @tasks.loop(time=time(hour=20, minute=0))
 async def daily_wife_message():
